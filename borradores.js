cb724b582e9d023faae053553099a6f5fcd2f57b
sebapitta <118943830+sebapitta@users.noreply.github.com>
2023-01-12 14:24:42 -0300

    primer commit

---
+++ borradores.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.listen(3002, () => {
+  console.log("Servidor funcionando en el puerto", app.listen);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get('/productos/:idProductos', function(req,res) {
+  res.send("Bienvenidos al detalle de producto");
+})
